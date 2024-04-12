59d590103cc55b066627dadafe485ec35ea5cc35
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2024-04-12 14:53:31 -0300

    Update av120212.c

--- repeticao/av120212.c
+++ repeticao/av120212.c
@@ -3,16 +3,15 @@
 int main()
 {
     char digito[100];
-    int i, j, aux;
-    int tamanho;
+    int i, j, aux, tam;
 
     printf("Insira o digito: ");
     scanf("%s", digito);
 
-    for(tamanho = 0; digito[tamanho] != '\0'; tamanho++);
+    for(tam = 0; digito[tam] != '\0'; tam++);
 
-    for(j = 0; j < tamanho-1; j++) {
-        for(i = 0; i < tamanho-j-1; i++) {
+    for(j = 0; j < tam-1; j++) {
+        for(i = 0; i < tam-j-1; i++) {
             if(digito[i] > digito[i+1]) {
                 aux = digito[i];
                 digito[i] = digito[i+1];
@@ -22,7 +21,7 @@ int main()
     }
 
     printf("Menor digito: %c\n", digito[0]);
-    printf("Maior digito: %c\n", digito[tamanho - 1]);
+    printf("Maior digito: %c\n", digito[tam - 1]);
 
     return 0;
 }
